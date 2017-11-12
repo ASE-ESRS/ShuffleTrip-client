9de7ad24f2208c23f905a2794d4a53d11e8244be
Loic Verrall <loic.verrall@gmail.com>
2017-11-12 04:49:01 +0000

    JSON decoding added. Button tint colour changed.

--- ShuffleTrip/ShuffleTrip/Controllers/ServerBackendController.swift
+++ ShuffleTrip/ShuffleTrip/Controllers/ServerBackendController.swift
@@ -21,19 +21,44 @@ public class ServerBackendController {
 	
 	
 	
+	// MARK: -
+	
 	func requestRandomTrip(completionHandler: @escaping (Trip) -> ()) {
-		let parameters: Parameters = ["userId" : phoneNumber]
-		Alamofire.request("https://iagx7vnad8.execute-api.eu-west-2.amazonaws.com/prod/shuffleTrip?route=trips/shuffle", method: .post, parameters: parameters, encoding: URLEncoding.queryString).responseJSON { jsonResponse in
-////			print(jsonResponse.result.value)
-//			let trip = Trip(destination: "BARCELONA", cost: 129.55, latitude: 41.390205, longitude: 2.154007)
-//			completionHandler(trip)
+		Alamofire.request("https://iagx7vnad8.execute-api.eu-west-2.amazonaws.com/prod/shuffleTrip?route=trips/shuffle", method: .get, encoding: JSONEncoding.default).responseJSON { response in
+			if let status = response.response?.statusCode {
+				switch status {
+				case 201:
+					print("[ServerBackendController]\t\(status): SUCCESS")
+					if let result = response.result.value {
+						let json = result as! Dictionary<String, Any>
+						print(json)
+						
+						// Extract the latitude and longitude.
+						let airportLocationString = json["airport_location"] as! String
+						let commaIndex = airportLocationString.index(of: ",")!
+						let longitude = "\(airportLocationString[commaIndex...])"
+						let latitude = "\(airportLocationString[..<commaIndex])"
+						
+						let trip = Trip(countryName: 	json["country_name"] as! String,
+										countryID: 		json["country"] as! String,
+										airportName: 	json["country_name"] as! String,
+										airportID: 		json["country_code"] as! String,
+										airportCoordinates: (longitude, latitude),
+										airportCity: 	json["country_city"] as! String,
+										cost: 			json["price"] as! Double)
+						completionHandler(trip)
+					}
+				default:
+					print("[ServerBackendController]\t\(status): ERROR — \(response)")
+				}
+			}
 		}
 	}
 	
 	func savePhoneNumber() {
 		let parameters: Parameters = ["userId" : phoneNumber]
 		Alamofire.request("https://4wmuzhlr5b.execute-api.eu-west-2.amazonaws.com/prod/StorePhoneNumber", method: .post, parameters: parameters, encoding: URLEncoding.queryString).response { response in
-			print("Request POSTed")
+			print("[ServerBackendController]\tPhone number POST request sent")
 		}
 	}
 
